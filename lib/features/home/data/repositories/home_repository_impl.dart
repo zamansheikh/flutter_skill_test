660016c26702cab0ceef53550deae610dce3ae8d
Zaman Sheikh <zaman6545@gmail.com>
2024-12-24 15:31:08 +0600

    implement network

--- lib/features/home/data/repositories/home_repository_impl.dart
+++ lib/features/home/data/repositories/home_repository_impl.dart
@@ -1,20 +1,27 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_skill_test/core/error/exception.dart';
 import 'package:flutter_skill_test/core/error/failure.dart';
+import 'package:flutter_skill_test/core/platform/network_info.dart';
 import 'package:flutter_skill_test/features/home/data/datasources/home_fake_remote_datasource.dart';
 import 'package:flutter_skill_test/features/home/domain/entities/house.dart';
 import 'package:flutter_skill_test/features/home/domain/repositories/home_repository.dart';
 
 class HomeRepositoryImpl implements HomeRepository {
   final HomeFakeRemoteDatasource datasource;
-  HomeRepositoryImpl({required this.datasource});
+  final NetworkInfo networkInfo;
+  HomeRepositoryImpl({required this.datasource, required this.networkInfo});
 
   @override
   Future<Either<Failure, List<House>>> fetchAllHouses() async {
-    try {
-      final result = await datasource.fetchAllHouse();
-      return Right(result);
-    } on ServerException {
+    if (await networkInfo.isConnected) {
+      try {
+        final result = await datasource.fetchAllHouse();
+        return Right(result);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+      //Load cache data is not Implemented
       return Left(ServerFailure());
     }
   }
--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,3 +1,5 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter_skill_test/core/platform/network_info.dart';
 import 'package:flutter_skill_test/features/home/data/datasources/home_fake_remote_datasource.dart';
 import 'package:flutter_skill_test/features/home/data/repositories/home_repository_impl.dart';
 import 'package:flutter_skill_test/features/home/domain/repositories/home_repository.dart';
@@ -9,13 +11,19 @@ import 'package:http/http.dart' as http;
 class Injection {
   static void init() {
     // External
+    Get.lazyPut(() => Connectivity());
     Get.lazyPut(() => http.Client());
 
+    // Core
+    Get.lazyPut<NetworkInfo>(() => NetworkInfoImpl(Get.find()));
+
     // Data Source
-    Get.lazyPut<HomeFakeRemoteDatasource>(() => HomeFakeRemoteDatasourceImpl(client: Get.find()));
+    Get.lazyPut<HomeFakeRemoteDatasource>(
+        () => HomeFakeRemoteDatasourceImpl(client: Get.find()));
 
     // Repository
-    Get.lazyPut<HomeRepository>(() => HomeRepositoryImpl(datasource: Get.find()));
+    Get.lazyPut<HomeRepository>(() =>
+        HomeRepositoryImpl(datasource: Get.find(), networkInfo: Get.find()));
 
     // Use Case
     Get.lazyPut(() => FetchAllHousesUsecase(repository: Get.find()));
